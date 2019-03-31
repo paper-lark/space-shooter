2565f9263939b0c734de04371b556b51a1cc8256
Max Zhuravsky <paperlark@yandex.com>
2019-03-31 15:48:59 +0300

    fix text shader

--- shaders/hud/fragment.glsl
+++ shaders/hud/fragment.glsl
@@ -5,8 +5,6 @@ uniform sampler2D text;
 uniform vec3 textColor;
 
 void main() {
-	texColor = vec4(textColor, 1.f);
-	return;
 	vec4 sampled = vec4(1.0, 1.0, 1.0, texture(text, TexCoords).r);
 	texColor = vec4(textColor, 1.0) * sampled;
 }
